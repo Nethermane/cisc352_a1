74d174169a2776643c5294e662f002158eeae426
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-02-18 00:43:09 -0500

    Implemented with 55 sec 3 mil algorithm

--- nqueen.py
+++ nqueen.py
@@ -1,140 +1,134 @@
 import datetime
-import math
 import random
 
-sample_input = [6000]
+sample_input = [10000000]
 
 
-def lcg(n, x0, a, c, m):
-    prs = []
-    xn = x0
-    for i in range(n):
-        xn = (a * xn + c) % m
-        prs.append(xn)
-    return prs
+def randint(min, max):
+    return int(random.random() * (max - min) + min)
 
 
-def increment_board_square(value):
-    if value >= 0:
-        return value + 1
-    else:
-        return value - 1
+def swap(board, j, m, pos_slope_diag, neg_slope_diag):
+    pos_slope_diag[board[m] - m] -= 1
+    neg_slope_diag[board[m] + m] -= 1
+    pos_slope_diag[board[j] - j] -= 1
+    neg_slope_diag[board[j] + j] -= 1
+    board[j], board[m] = board[m], board[j]
+    pos_slope_diag[board[m] - m] += 1
+    neg_slope_diag[board[m] + m] += 1
+    pos_slope_diag[board[j] - j] += 1
+    neg_slope_diag[board[j] + j] += 1
 
 
-def get_min_val_in_column(board_size, left_diag, right_diag, row_dict, col):
-    if col < board_size // 2:
-        return col*2
-    min_val = board_size + 1
-    min_pos = []
-    for row in range(board_size):
-        sum_pos = left_diag[row - col] + right_diag[row + col] + row_dict[row]
-        if sum_pos < min_val:
-            min_val = sum_pos
-            min_pos = [row]
-        elif sum_pos == min_val:
-            min_pos.append(row)
-        if min_val == 0:
-            break
-    return random.choice(min_pos)
+# Largely inspired by techniques presented in: http://algolist.ru/download.php?path=/maths/combinat/ieeekde94.zip&pspdf=1&ask=1
+# If you draw this out the column is distance from bottom, not form top
+'''
+Parameters
+    n = Board size
+
+Returns:
+    k = number of queen's place "dangerously" (without enforcing diagonal non-collision)
+    board = the board that was generated
+    pos_slope_diag = Dictionary representing the collisions on each positive diagonal
+    neg_slope_diag = Dictionary representing the collisions on each negative diagonal
+'''
 
 
-# def increment_diagonal(board, row_to_increment, col_to_increment):
-#     for k in range(min_row, input_size):
-#         # increment diagonal
-#         increment_board_square(broken_queens[i + k][min_row + k])
+def create2(n):
+    board = [i for i in range(n)]  # Column index is indicated by list index, row index is the value at that index
+    pos_slope_diag = {i: 0 for i in range(-n + 1, n)}  # up to left
+    neg_slope_diag = {i: 0 for i in range(n * 2)}  # up to right
+    j = 0  # The current index it is trying to place properly
+    n_minus_one = n - 1
+    # This loop is placing collisionless queen's
+    for i in range(int(3.08 * n)):  # 3.08*n is the average number of attempted insertions to get a complete board
+        # Default random.randint() was very slow so has been replaced
+        m = randint(j, n_minus_one)
+        # Check if swapping the current column with m column causes no collisions
+        # Can completely ignore rows because the swaps will ensure they are on different rows
+        if pos_slope_diag[board[m] - j] == 0 and neg_slope_diag[board[m] + j] == 0:
+            # Do this one way swap wherein the present column, j, is the only one added to the diagonal dicts
+            # The second one will get added later in this loop or in the below loop
+            pos_slope_diag[board[m] - j] += 1
+            neg_slope_diag[board[m] + j] += 1
+            board[j], board[m] = board[m], board[j]  # Swapping them ensures row collisions will never be an issue
+            j += 1  # Move to next column
+            # Prevent index out of bounds issues if it manages to find the perfect solution first try
+            if j == n:
+                return n - j, board, pos_slope_diag, neg_slope_diag
+    # This loop is placing queens that almost certainly have collisions
+    for i in range(j, n):
+        rand_swap = randint(i, n_minus_one)  # Just choose randomly from the future columns as to where to place
+        pos_slope_diag[board[rand_swap] - i] += 1
+        neg_slope_diag[board[rand_swap] + i] += 1
+        board[i], board[rand_swap] = board[rand_swap], board[i]
+    return n - j, board, pos_slope_diag, neg_slope_diag
 
-def create(board_size):
-    board_state = list()
-    left_diag = {i: 0 for i in range(-board_size + 1, board_size)}  # up to left
-    right_diag = {i: 0 for i in range(board_size * 2 - 1)}  # up to right
-    row_dict = {i: 0 for i in range(board_size)}
-    for col in range(board_size):
-        selected_min_pos = get_min_val_in_column(board_size, left_diag, right_diag, row_dict, col)
-        # Add to actual board array
-        board_state.append((selected_min_pos, col))
-        # left diag
-        left_diag[selected_min_pos - col] += 1
-        # right diag
-        right_diag[selected_min_pos + col] += 1
-        # row
-        row_dict[selected_min_pos] += 1
-    return board_state, left_diag, right_diag, row_dict
+
+'''
+Corrects a board or fails (if the board is decided to be too complex) or un-fixible by this algorithm
+Parameters:
+    n = board size
+    board = the board to correct
+    pos_slope_diag = Dictionary representing the collisions on each positive diagonal
+    neg_slope_diag = Dictionary representing the collisions on each negative diagonal
+Returns:
+    whether the board was fixed
+'''
+
+
+def fix(n, k, board, pos_slope_diag, neg_slope_diag):
+
+    for i in range(n - k):
+        if pos_slope_diag[board[i] - i] > 1 or neg_slope_diag[board[i] + i] > 1:
+            count = 0
+            while True:
+                m = randint(0, n - 1)
+                swap(board, i, m, pos_slope_diag, neg_slope_diag)
+                if (pos_slope_diag[board[i] - i] == 1 and neg_slope_diag[board[i] + i] == 1 and pos_slope_diag[
+                    board[m] - m] == 1 and neg_slope_diag[board[m] + m] == 1):
+                    break
+                swap(board, i, m, pos_slope_diag, neg_slope_diag)
+                count += 1
+                if count > 7000:
+                    return False
+    return True
 
 
 def main():
-    queens_to_return = []
-    for input_size in sample_input:
-        startTime = datetime.datetime.now()
-        board, left_diag, right_diag, row_dict = create(input_size)
-        print("Generated first board in ", datetime.datetime.now() - startTime, "seconds")
-        # queen_list = [
-        #     ((len(left_diag[row - col]) + len(right_diag[row + col]) + len(row_dict[row])) - 3, row, col) for (row, col)
-        #     in
-        #     board]  # col, collisions
-        # queen_heap = []
-        # for queen in queen_list:
-        #     heapq.heappush(queen_heap)
-        # while len(queen_heap) > 0:
-        #     print(heapq.heappop(queen_heap))
-        loops = 0
-        while 1:
-            loops += 1
-            if loops == math.sqrt(input_size) + 20:
-                board, left_diag, right_diag, row_dict = create(input_size)
-                print('reset')
-                loops = 0
-            all_collisions = [
-                (left_diag[x[0] - x[1]] + right_diag[x[0] + x[1]] + row_dict[x[0]], x[0], x[1]) for x in
-                board]
-            biggest = max(all_collisions)  # max default goes on first index
-            # collisions = len(left_diag[biggest[0] - biggest[1]]) + len(right_diag[biggest[0] + biggest[1]]) + len(
-            #     row_dict[biggest[0]])
-            all_big = [s for s in all_collisions if s[0] == biggest[0]]
-            biggest = random.choice(all_big)
-            row = biggest[1]
-            col = biggest[2]
-            if biggest[0] == 3:  # collisions on itself == 3
-                print(board)
-                break
-            # finding minimum position
-            min_val = input_size + 1
-            min_pos = []
-            for rows_to_insert in range(input_size):
-                left_val = left_diag[rows_to_insert - col]
-                right_val = right_diag[rows_to_insert + col]
-                row_val = row_dict[rows_to_insert]
-                sum_pos = left_val + right_val + row_val
-                if sum_pos < min_val:
-                    min_val = sum_pos
-                    min_pos = [rows_to_insert]
-                elif sum_pos == min_val:
-                    min_pos.append(rows_to_insert)
-                if min_val == 0:
-                    break
-            # This is bad complexity, fix it later
-            # FIX ALL OF THE COMPLEXITY HERE
-            board.remove((row, col))
-            # left diag
-            left_diag[row - col] -= 1
-            # right diag
-            right_diag[row + col] -= 1
-            # row
-            row_dict[row] -= 1
-
-            # Choose a random one of the minimum options
-            to_move_to = random.choice(min_pos)
-
-            # Add to actual board array
-            board.append((to_move_to, col))
-            # left diag
-            left_diag[to_move_to - col] += 1
-            # right diag
-            right_diag[to_move_to + col] += 1
-            # row
-            row_dict[to_move_to] += 1
-
-        print("Solved board in ", datetime.datetime.now() - startTime, "seconds")
-    return
-
+    found = False
+    startTime = datetime.datetime.now()
+    iterations = 0
+    while not found:
+        k, board, pos_slope_dict, neg_slop_dict = create2(sample_input[0])
+        found = fix(sample_input[0], k, board, pos_slope_dict, neg_slop_dict)
+        iterations += 1
+    print("Generated board in ", datetime.datetime.now() - startTime, "seconds and", iterations, "iterations")
+    return iterations
+#
+# sample_input = [10000]
+# startTime = datetime.datetime.now()
+# print("avg iterations for 10,000", sum([main() for i in range(10)])/10)
+# print("Generated board in ", datetime.datetime.now() - startTime)
+#
+# sample_input = [100000]
+# startTime = datetime.datetime.now()
+# print("avg iterations for 100,000", sum([main() for i in range(10)])/10)
+# print("Generated board in ", datetime.datetime.now() - startTime)
+#
+# sample_input = [1000000]
+# startTime = datetime.datetime.now()
+# print("avg iterations for 1,000,000", sum([main() for i in range(10)])/10)
+# print("Generated board in ", datetime.datetime.now() - startTime)
+#
+# sample_input = [2000000]
+# startTime = datetime.datetime.now()
+# print("avg iterations for 2,000,000", sum([main() for i in range(10)])/10)
+# print("Generated board in ", datetime.datetime.now() - startTime)
+#
+# sample_input = [3000000]
+# startTime = datetime.datetime.now()
+# print("avg iterations for 3,000,000", sum([main() for i in range(10)])/10)
+# print("Generated board in ", datetime.datetime.now() - startTime)
 
 main()
